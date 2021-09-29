3b2a456b05d7f0b16025469062395260d0981628
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2021-09-29 18:31:11 +0530

    Add files via upload

---
+++ Number Of Open Doors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int noOfOpenDoors(long long N) 
+    {
+        // code here
+        int ans=0;
+        
+        ans=floor(sqrt(N));
+        
+        return ans;
+    }
+};
